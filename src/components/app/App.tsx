504c017182c62e43db694c70be488467d69618ab
tim <stafr95@mail.ru>
2024-12-10 14:53:11 +0500

    init proj

---
+++ src/components/app/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
+import TodoFilter from './components/TodoFilter';
+
+
+
+export default App
