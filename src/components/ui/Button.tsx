40582ccc9608160a8a98a0fbb2c75f99ed6e23b7
tim <stafr95@mail.ru>
2024-12-11 14:25:58 +0500

    Creating a Reusable Component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './button.scss';
+
+interface ButtonProps {
+	onClick: () => void;
+	className?: string,
+	children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, className, children }) => {
+	return (
+		<button onClick={onClick} className={className}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
