eea133ee714750af4e9787e5a43128d5932999d4
tim <stafr95@mail.ru>
2024-12-11 16:35:03 +0500

    Import vitest

--- src/tests/todo.test.tsx
+++ src/tests/todo.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import { expect, describe, test, } from 'vitest';
 import App from '../components/app/App.tsx';
 
 describe('Todo App', () => {
